8be01d3255b7d088b0468ccf45b3979c833ca258
Peter Chen <peterchen2017125@gmail.com>
2025-04-09 21:41:16 +0800

    feat: add new logo

--- src/data/data.ts
+++ src/data/data.ts
@@ -12,40 +12,17 @@ const sponsors = [
     link: "https://cokedesign.com.tw/",
   },
   {
-    name: "國立宜蘭大學",
-    logo: "/logo/1.jpg",
-    url: "../../public/logo/1.jpg",
-    link: "https://cokedesign.com.tw/",
+    name: "​伯熱音悅娛樂",
+    logo: "/logo/3.png",
+    url: "../../public/logo/3.png",
+    link: "https://www.bohotofficial.com/",
   },
+
   {
-    name: "國立宜蘭大學",
-    logo: "/logo/2.jpg",
-    url: "../../public/logo/2.jpg",
-    link: "https://cokedesign.com.tw/",
-  },
-  {
-    name: "國立宜蘭大學",
-    logo: "/logo/1.jpg",
-    url: "../../public/logo/1.jpg",
-    link: "https://cokedesign.com.tw/",
-  },
-  {
-    name: "國立宜蘭大學",
-    logo: "/logo/2.jpg",
-    url: "../../public/logo/2.jpg",
-    link: "https://cokedesign.com.tw/",
-  },
-  {
-    name: "國立宜蘭大學",
-    logo: "/logo/1.jpg",
-    url: "../../public/logo/1.jpg",
-    link: "https://cokedesign.com.tw/",
-  },
-  {
-    name: "國立宜蘭大學",
-    logo: "/logo/2.jpg",
-    url: "../../public/logo/2.jpg",
-    link: "https://cokedesign.com.tw/",
+    name: "HORSE LAI",
+    logo: "/logo/4.png",
+    url: "../../public/logo/4.png",
+    link: "https://www.horselai.com/",
   },
 ];
 

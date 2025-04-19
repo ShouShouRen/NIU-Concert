594c2ab93be04cbd1c08f5511eccc3c4d6caa72b
Peter Chen <peterchen2017125@gmail.com>
2025-04-19 15:05:35 +0800

    feat: add new logo

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,27 +2,33 @@ const sponsors = [
   {
     name: "微星科技MSI",
     logo: "/logo/1.jpg",
-    url: "../../public/logo/1.jpg",
     link: "https://tw.msi.com/",
   },
   {
     name: "酷可創念",
     logo: "/logo/2.jpg",
-    url: "../../public/logo/2.jpg",
     link: "https://cokedesign.com.tw/",
   },
   {
     name: "​伯熱音悅娛樂",
     logo: "/logo/3.png",
-    url: "../../public/logo/3.png",
     link: "https://www.bohotofficial.com/",
   },
 
   {
     name: "HORSE LAI",
     logo: "/logo/4.png",
-    url: "../../public/logo/4.png",
     link: "https://www.horselai.com/",
+  },
+  {
+    name: "HARU 含春",
+    logo: "/logo/5.png",
+    link: "https://letsharu.cc/KFbSe",
+  },
+  {
+    name: "ELLO 研舒肌",
+    logo: "/logo/6.png",
+    link: "https://ellohello.cc/h8JRD",
   },
 ];
 
